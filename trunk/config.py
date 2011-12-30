219c3453c6c6e554e281ccfec853068bac0a3501
sergzach <sergzach@6fc61962-dde3-eefd-1c1b-c2d9e354a1c0>
2011-12-30 22:33:47 +0000

    + support of multiple configs

--- trunk/config.py
+++ trunk/config.py
@@ -1,15 +1,18 @@
-config = {	'html_extentions': ( "tmpl", ), 
-			'js_extentions': ( "js", ), 
-			'static_extentions': ( "css", ),
-			'source_dir': "/root/dargent/",
-			'result_dir': "/www/dargent_root/dargent/",
-			'get_sources_command': "svn checkout http://dargent.googlecode.com/svn/trunk/dargent/ /root/dargent/",
-			'stop_commands': "/etc/init.d/apache2 stop; /usr/local/nginx/sbin/nginx -s stop",
-			'start_commands': "/usr/local/nginx/sbin/nginx; /etc/init.d/apache2 start",
-			'salt_path': './salt',
-			'additional_replaces':	{
-										'settings.py': ( "localhost", "mosdargent.ru" )
-									}
-		}	
+configs = 
+{
+	'dargent-ru':	{	'html_extentions': ( "html", ), 
+						'js_extentions': ( "js", ), 
+						'static_extentions': ( "css", ),
+						'source_dir': "/root/dargent/",
+						'result_dir': "/www/dargent_root/dargent/",
+						'get_sources_command': "svn checkout http://dargent-ru.googlecode.com/svn/trunk/dargent/ /root/dargent/",
+						'stop_commands': "/etc/init.d/apache2 stop;",
+						'start_commands': "chown -R www-data /www/dargent_root/dargent/static/user/; /usr/local/nginx/sbin/nginx -s reload; /etc/init.d/apache2 start",
+						'salt_path': './salt',
+						'additional_replaces':	{
+													'settings.py': ( "localhost", "mosdargent.ru" )
+												}
+					}
+}						
 
 
