b6920b80462ce3010a69552058f4e477312a631a
sergzach <sergzach@6fc61962-dde3-eefd-1c1b-c2d9e354a1c0>
2011-12-30 22:40:12 +0000

    min. corr

--- build.py
+++ build.py
@@ -3,5 +3,9 @@ from config import config
 from sys import argv
 
 if __name__ == "__main__":
-	sB = SiteBuilder( **config[ argv[ 1 ] ] )
+	try:
+		sB = SiteBuilder( **config[ argv[ 1 ] ] )
+	except IndexError:
+		print "Usage: python build.py config_key"
+		exit()
 	sB.build()
