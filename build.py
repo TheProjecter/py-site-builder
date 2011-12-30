dd50206b44d8ce1e29c38248ac569008bc05f94b
sergzach <sergzach@6fc61962-dde3-eefd-1c1b-c2d9e354a1c0>
2011-12-30 22:33:47 +0000

    + support of multiple configs

--- build.py
+++ build.py
@@ -1,6 +1,7 @@
 from sitebuilder import SiteBuilder
 from config import config
+from sys import argv
 
 if __name__ == "__main__":
-	sB = SiteBuilder( **config )
+	sB = SiteBuilder( **config[ argv[ 1 ] ] )
 	sB.build()
